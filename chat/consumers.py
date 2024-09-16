fbc8c231512bfe7cc656c2482d132aac30681769
MaAri <MaAriec@yandex.ru>
2024-09-16 19:27:37 +0300

    Exception inside application: Group name must be a valid unicode string containing only ASCII alphanumerics, hyphens, or periods. Traceback (most recent call last):

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,23 +1,33 @@
 import json
+from datetime import datetime
+
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+# from models import Message
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        self.room_group_name = 'test'
+        self.room_group_name = self.scope['url_route']['kwargs']['chat_id']
 
+        print(self.room_group_name)
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
 
         self.accept()
-   
+
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
+        # message_object = Message.objects.create(
+        #     text=message,
+        #     author=self.scope['user'],
+        #     date=datetime.datetime.now(),
+        #     room=self.room_group_name
+        #
+        # )
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
--- djangoProject4/asgi.py
+++ djangoProject4/asgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -18,9 +19,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject4.settings')
 
 application = ProtocolTypeRouter({
     'http':get_asgi_application(),
-    'websocket':AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    )
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+    ),
 })
