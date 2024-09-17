ec7121ca492c2502ccb112d8962ca2b36e7925e0
MaAri <MaAriec@yandex.ru>
2024-09-17 17:53:04 +0300

    Chat logic has been created

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,35 +1,36 @@
 import json
 from datetime import datetime
-
+from .models import Message, Room
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 # from models import Message
 
+
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        self.room_group_name = self.scope['url_route']['kwargs']['chat_id']
 
-        print(self.room_group_name)
+        self.room_id = self.scope['path'].split('/')[-2]
+        print(self.room_id)
+
         async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
+            self.room_id,
             self.channel_name
         )
 
         self.accept()
 
-
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        # message_object = Message.objects.create(
-        #     text=message,
-        #     author=self.scope['user'],
-        #     date=datetime.datetime.now(),
-        #     room=self.room_group_name
-        #
-        # )
+        message_object = Message.objects.create(
+            text=message,
+            author=self.scope['user'],
+            date=datetime.now(),
+            room=Room.objects.get(pk=int(self.room_id)),
+
+        )
         async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
+            self.room_id,
             {
                 'type':'chat_message',
                 'message':message
